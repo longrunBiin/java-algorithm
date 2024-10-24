cc855fdf43c279a899506bde343126c5f321d7e7
jangsubin <eltm521@naver.com>
2024-10-24 17:55:32 +0900

    15688 clear

--- src/main/java/org/example/baekjoon/silver/Num15688.java
+++ src/main/java/org/example/baekjoon/silver/Num15688.java
@@ -1,2 +1,26 @@
-package org.example.baekjoon.silver;public class Num15688 {
+package org.example.baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+
+public class Num15688 {
+    static int n;
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = (Integer.parseInt(br.readLine()));
+        }
+
+        Arrays.sort(nums);
+        for (int i = 0; i < n; i++) {
+            sb.append(nums[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
 }
