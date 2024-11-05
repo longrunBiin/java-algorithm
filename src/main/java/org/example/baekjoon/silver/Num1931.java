9cec97f850599bc88318fcdd20651caf8d2b4c3b
jangsubin <eltm521@naver.com>
2024-11-05 16:07:46 +0900

    1931 clear

--- src/main/java/org/example/baekjoon/silver/Num1931.java
+++ src/main/java/org/example/baekjoon/silver/Num1931.java
@@ -20,14 +20,11 @@ public class Num1931 {
             time[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(time, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1[1] == o2[1]) {
-                    return o1[0] = o2[0];
-                }
-                return o1[1] - o2[1];
+        Arrays.sort(time, (o1, o2) -> {
+            if (o1[1] == o2[1]) {
+                return o1[0] - o2[0];
             }
+            return o1[1] - o2[1];
         });
 
         int count = 0;
