64d8c542bda47c66f5a889d9bf9f5758df4b6e18
jangsubin <eltm521@naver.com>
2024-11-05 16:05:39 +0900

    1931 clear

---
+++ src/main/java/org/example/baekjoon/silver/Num1931.java
@@ -0,0 +1,48 @@
+package org.example.baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Num1931 {
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        int[][] time = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            time[i][0] = Integer.parseInt(st.nextToken());
+            time[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(time, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return o1[0] = o2[0];
+                }
+                return o1[1] - o2[1];
+            }
+        });
+
+        int count = 0;
+        int prev_end_time = 0;
+
+        for(int i = 0; i < n; i++) {
+
+            // 직전 종료시간이 다음 회의 시작 시간보다 작거나 같다면 갱신
+            if(prev_end_time <= time[i][0]) {
+                prev_end_time = time[i][1];
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+}
