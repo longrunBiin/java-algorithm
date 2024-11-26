7d8cc45883e2fce06256b5576980ffe2b25f9aab
jangsubin <eltm521@naver.com>
2024-11-26 20:54:12 +0900

    programmers 42577 clear

---
+++ src/main/java/org/example/programmers/level2/Num42577.java
@@ -0,0 +1,26 @@
+package org.example.programmers.level2;
+import java.util.*;
+
+public class Num42577 {
+
+    class Solution {
+        public boolean solution(String[] phone_book) {
+            boolean answer = true;
+            HashMap<String, Integer> map = new HashMap<>();
+
+            for(int i=0; i<phone_book.length; i++) {
+                map.put(phone_book[i], 1);
+            }
+
+            for(int i=0; i<phone_book.length; i++) {
+                for(int j=0; j<phone_book[i].length(); j++) {
+                    if(map.containsKey(phone_book[i].substring(0, j))) {
+                        return false;
+                    }
+                }
+            }
+
+            return answer;
+        }
+    }
+}
