2767110b5543a40d1d13fb197bbb5ccc8fd9dc59
jangsubin <eltm521@naver.com>
2025-02-12 12:32:52 +0900

    68644 수정

--- src/main/java/org/example/programmers/level1/Num68644.java
+++ src/main/java/org/example/programmers/level1/Num68644.java
@@ -2,17 +2,18 @@ package org.example.programmers.level1;
 
 import java.util.*;
 
-class Solution {
-    public int[] solution(int[] numbers) {
-        HashSet<Integer> set = new HashSet<>();
-        int a = 0;
-        for(int index = 0; index < numbers.length; index++){
-            for(int i=index + 1;i<numbers.length;i++){
-                set.add(numbers[index] + numbers[i]);
+class Num68644 {
+    class Solution {
+        public int[] solution(int[] numbers) {
+            HashSet<Integer> set = new HashSet<>();
+            int a = 0;
+            for (int index = 0; index < numbers.length; index++) {
+                for (int i = index + 1; i < numbers.length; i++) {
+                    set.add(numbers[index] + numbers[i]);
+                }
             }
+
+            return set.stream().sorted().mapToInt(Integer::intValue).toArray();
         }
-
-
-        return set.stream().sorted().mapToInt(Integer::intValue).toArray();
     }
 }
