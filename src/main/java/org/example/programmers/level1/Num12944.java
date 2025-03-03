d1313ba448fcbe7965c2e463c66a9cf0449f91ab
jangsubin <eltm521@naver.com>
2025-03-03 22:38:33 +0900

    12944 clear

---
+++ src/main/java/org/example/programmers/level1/Num12944.java
@@ -0,0 +1,14 @@
+package org.example.programmers.level1;
+
+public class Num12944 {
+    class Solution {
+        public double solution(int[] arr) {
+            double answer = 0;
+            for(int i=0; i<arr.length;i++){
+                answer += arr[i];
+            }
+
+            return answer / arr.length;
+        }
+    }
+}
