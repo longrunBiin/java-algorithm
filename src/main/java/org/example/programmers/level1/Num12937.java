be457ab8a74b6ff69e10435035a22c113a21c6df
jangsubin <eltm521@naver.com>
2025-03-03 22:37:23 +0900

    12937 clear

---
+++ src/main/java/org/example/programmers/level1/Num12937.java
@@ -0,0 +1,10 @@
+package org.example.programmers.level1;
+
+public class Num12937 {
+    class Solution {
+        public String solution(int num) {
+            if(num%2 == 0) return "Even";
+            return "Odd";
+        }
+    }
+}
