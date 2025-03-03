135de4401d51f10168a1268828dcb998ed62b657
jangsubin <eltm521@naver.com>
2025-03-03 22:36:16 +0900

    12931 clear

---
+++ src/main/java/org/example/programmers/level1/Num12931.java
@@ -0,0 +1,16 @@
+package org.example.programmers.level1;
+
+public class Num12931 {
+    public class Solution {
+        public int solution(int n) {
+            int answer = 0;
+
+            while(n != 0){
+                answer += n%10;
+                n/=10;
+            }
+
+            return answer;
+        }
+    }
+}
