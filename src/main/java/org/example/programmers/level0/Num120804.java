b2f3a9f2ed631eac321a04613705a6715c5d1156
jangsubin <eltm521@naver.com>
2025-03-15 22:10:51 +0900

    120804 clear

---
+++ src/main/java/org/example/programmers/level0/Num120804.java
@@ -0,0 +1,9 @@
+package org.example.programmers.level0;
+
+public class Num120804 {
+    class Solution {
+        public int solution(int num1, int num2) {
+            return num1 * num2;
+        }
+    }
+}
