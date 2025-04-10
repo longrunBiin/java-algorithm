10932b43a88310d8e0b7b81f7f662591e93366ef
subin <eltm521@naver.com>
2025-04-10 20:04:56 +0900

    42577 again

--- src/main/java/org/example/programmers/level2/Num42577.java
+++ src/main/java/org/example/programmers/level2/Num42577.java
@@ -23,4 +23,15 @@ public class Num42577 {
             return answer;
         }
     }
+    class Solution2 {
+        public boolean solution(String[] phone_book) {
+            Arrays.sort(phone_book);
+
+            for(int i=0;i<phone_book.length-1;i++){
+                if (phone_book[i + 1].startsWith(phone_book[i]))
+                    return false;
+            }
+            return true;
+        }
+    }
 }
