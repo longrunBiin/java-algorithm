f61af582974b4fc7ca36aaab9a6168973ad565a6
subin <eltm521@naver.com>
2025-04-14 20:02:13 +0900

    12980 clear

---
+++ src/main/java/org/example/programmers/level2/Num12980.java
@@ -0,0 +1,16 @@
+package org.example.programmers.level2;
+
+public class Num12980 {
+    public class Solution {
+        public int solution(int n) {
+            int ans = 0;
+
+            while(n!=0){
+                if(n%2 != 0) ans++;
+                n/=2;
+            }
+
+            return ans;
+        }
+    }
+}
