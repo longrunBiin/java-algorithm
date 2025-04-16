d988d0552abdfdfdeb175e9c84e74eba7cf7921b
subin <eltm521@naver.com>
2025-04-16 21:42:59 +0900

    12900 clear

---
+++ src/main/java/org/example/programmers/level2/Num12900.java
@@ -0,0 +1,19 @@
+package org.example.programmers.level2;
+
+import java.util.*;
+public class Num12900 {
+    class Solution {
+        public int solution(int n) {
+            int answer = 0;
+            int[] dp = new int[n+1];
+            Arrays.fill(dp, 0);
+            dp[1] = 1;
+            dp[2] = 2;
+
+            for(int i=3; i<= n;i++){
+                dp[i] = (dp[i-1] + dp[i-2]) % 1000000007;
+            }
+            return dp[n];
+        }
+    }
+}
