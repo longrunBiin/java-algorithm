652a09ee07982c1c6dcafdbae39a4606e33584c4
subin <eltm521@naver.com>
2025-04-23 20:13:50 +0900

    12982 clear

---
+++ src/main/java/org/example/programmers/level1/Num12982.java
@@ -0,0 +1,22 @@
+package org.example.programmers.level1;
+
+import java.util.*;
+public class Num12982 {
+    class Solution {
+        public int solution(int[] d, int budget) {
+            int answer = 0;
+            Arrays.sort(d);
+
+            for(int i=0;i<d.length;i++){
+
+                if(d[i] <= budget){
+                    budget-=d[i];
+                    answer++;
+                }else{
+                    break;
+                }
+            }
+            return answer;
+        }
+    }
+}
